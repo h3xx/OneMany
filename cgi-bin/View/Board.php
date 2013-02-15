6cce01fc52bc0f2f932465033f1c7435cd42c098
Dan Church <amphetamachine@gmail.com>
2013-02-15 12:26:39 -0600

    Begin work on the board view.

---
+++ cgi-bin/View/Board.php
@@ -0,0 +1,13 @@
+<?php
+
+class ViewBoard {
+	private static $icons = [
+		    "./images/thimble.jpg",      // index 0
+		    "./images/battleship.jpg",      // index 1
+		    "./images/car.jpg",      // index 2
+		    "./images/tophat.jpg",      // index 3
+		    "./images/dog.jpg",      // index 4
+		    "./images/wheelbarrow.jpg",      // index 5
+		    "./images/rider.jpg"      // index 6
+	];
+}
