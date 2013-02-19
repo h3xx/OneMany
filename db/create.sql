a58c9a9b3d6f11c455f6770de500c0a7f8d73265
Dan Church <amphetamachine@gmail.com>
2013-02-19 14:32:48 -0600

    Default game state should be 0.

--- db/create.sql
+++ db/create.sql
@@ -20,7 +20,7 @@ CREATE EXTENSION pgcrypto; -- needed for cryptographic functions
 CREATE TABLE game
 (
   game_id serial NOT NULL,
-  game_state integer NOT NULL,
+  game_state integer NOT NULL DEFAULT 0,
   game_name character varying(1024) NOT NULL,
   CONSTRAINT game_pkey PRIMARY KEY (game_id)
 )
