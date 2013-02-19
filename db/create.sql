3b4b28728bfe21cd6babf48d3aa0961ce9daad54
Dan Church <amphetamachine@gmail.com>
2013-02-19 09:21:44 -0600

    Add game_update table.

--- db/create.sql
+++ db/create.sql
@@ -81,3 +81,21 @@ CREATE TABLE chat
 WITH (
   OIDS=FALSE
 );
+
+-- Table: game_update
+
+-- DROP TABLE game_update;
+
+CREATE TABLE game_update
+(
+  game_id integer NOT NULL,
+  game_newstate integer NOT NULL,
+  game_change character varying(4096) NOT NULL,
+  CONSTRAINT game_update_pkey PRIMARY KEY (game_id, game_newstate),
+  CONSTRAINT game_update_game_id_fkey FOREIGN KEY (game_id)
+      REFERENCES game (game_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
