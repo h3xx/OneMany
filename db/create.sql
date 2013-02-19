96d4f3172ac95ea0ead6f0c95088f2e39531d375
Dan Church <amphetamachine@gmail.com>
2013-02-19 14:43:22 -0600

    chat.chat_time should default to now().

--- db/create.sql
+++ db/create.sql
@@ -91,7 +91,7 @@ CREATE TABLE chat
   chat_id serial NOT NULL,
   user_id integer NOT NULL,
   chat_text character varying(1024) NOT NULL,
-  chat_time timestamp without time zone NOT NULL,
+  chat_time timestamp without time zone NOT NULL DEFAULT now(),
   CONSTRAINT chat_pkey PRIMARY KEY (chat_id),
   CONSTRAINT chat_game_id_fkey FOREIGN KEY (game_id)
       REFERENCES game (game_id) MATCH SIMPLE
