0bc86ef05b9c1d906679e3697c554edd13095351
Dan Church <amphetamachine@gmail.com>
2013-02-19 15:38:56 -0600

    Fix syntax error.

--- cgi-bin/Controller/Chat.php
+++ cgi-bin/Controller/Chat.php
@@ -3,7 +3,7 @@
 class ControllerChat {
 	private $model, $user_id;
 
-	__construct ($model, $user_id) {
+	function __construct ($model, $user_id) {
 		$this->model = $model;
 		$this->user_id = $user_id;
 	}
