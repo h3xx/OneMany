5da835cbc46fc346c4c9dca3c25ac50896f69b5b
Dan Church <amphetamachine@gmail.com>
2013-02-19 15:40:08 -0600

    Flush out View class.

--- cgi-bin/View.php
+++ cgi-bin/View.php
@@ -3,5 +3,39 @@
 require_once('View/AjaxResponse.php');
 
 class View {
+	private $model;
 
+	private $ajr;
+
+	function __construct ($model) {
+		$this->model = $model;
+	}
+
+	public function processInstruction ($instr) {
+		switch ($instr['func']) {
+			case 'pollChat':
+				return $this->getAjaxResponse()->getChatUpdate($instr['args']);
+				;;
+			case 'pollGame':
+				return $this->getAjaxResponse()->getBoardUpdateInstructions($instr['args']);
+				;;
+
+		}
+	}
+
+	private function getAjaxResponse () {
+		if (!isset($this->ajr)) {
+			$this->ajr = new ViewAjaxResponse($this->model);
+		}
+		return $this->ajr;
+	}
+
+	function __get ($name) {
+		switch ($name) {
+			case 'ajr':
+				return $this->getAjaxResponse();
+				break;
+				;;
+		}
+	}
 }
