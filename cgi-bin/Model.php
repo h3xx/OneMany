f1778f7d89ae8f569acd82b72f01009f471cba16
Dan Church <amphetamachine@gmail.com>
2013-02-19 14:27:45 -0600

    Add Chat as part of the model.

--- cgi-bin/Model.php
+++ cgi-bin/Model.php
@@ -2,6 +2,7 @@
 
 require_once('Model/Database.php');
 require_once('Model/Game.php');
+require_once('Model/Chat.php');
 
 class Model {
 	private $dbi;
@@ -38,10 +39,24 @@ class Model {
 		return $this->game;
 	}
 
+	private function getChat () {
+		if (!isset($this->chat)) {
+			$this->chat = new ModelChat($this, $this->game_id);
+		}
+		return $this->chat;
+	}
+
 	function __get ($name) {
 		switch ($name) {
 			case 'game':
 				return $this->getGame();
+				;;
+			case 'chat':
+				return $this->getChat();
+				;;
+			case 'dbi':
+				# XXX : debugging
+				return $this->getDbi();
 				;;
 		}
 	}
---
+++ cgi-bin/Model/Chat.php
@@ -0,0 +1,72 @@
+<?php
+
+class ModelChat {
+	private $model, $game_id;
+
+	function __construct ($model, $game_id) {
+		$this->model = $model;
+		$this->game_id = $game_id;
+	}
+
+	function init () {
+		# XXX : method stub
+		#       ( Nothing to do here! )
+		#       v
+		#    /\o
+		#     /\/
+		#    /\
+		#   /  \
+		#  LOL LOL
+	}
+
+	# initial creation inside the database
+	function create () {
+	}
+
+	public function getChatUpdates ($chat_last) {
+		$sth = $this->model->prepare(
+			'select "user_name" as "user", '.
+				'"chat_text" as "text", '.
+				'"chat_time" as "time", '.
+				'"chat_id" from ('.
+
+				'"chat" left join "user" on ('.
+					'"user"."user_id" = "chat"."user_id"'.
+				')'.
+			') '.
+			'where "game_id" = :gid and "chat_id" > :cid '.
+			'order by "chat_time" asc'
+		);
+
+		$sth->bindParam(':gid', $this->game_id, PDO::PARAM_INT);
+		$sth->bindParam(':cid', $chat_last, PDO::PARAM_INT);
+
+		# gets set to the (1-indexed) column last fetched row
+		$newstate = 0;
+		$sth->bindColumn(4, $newstate);
+
+		$sth->execute();
+
+		$result = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+		# limit result set
+		$onlywant = ['user'=>0,'text'=>0,'time'=>0];
+		$instructions = [];
+
+		foreach ($result as $row) {
+			$instructions []= array_intersect_key($row, $onlywant);
+		}
+
+		$data = [
+			'instructions'	=> $instructions,
+			'newstate'	=> $newstate,
+		];
+
+		return $data;
+	}
+
+	function __get ($name) {
+		switch ($name) {
+		}
+	}
+}
