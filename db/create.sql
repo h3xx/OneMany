0134fa22e266e24ff04026f39ac614ce2a0593e6
Dan Church <amphetamachine@gmail.com>
2013-02-19 09:26:51 -0600

    Revise chat table.

--- db/create.sql
+++ db/create.sql
@@ -71,12 +71,15 @@ COMMENT ON TABLE commchest
 
 CREATE TABLE chat
 (
+  game_id integer NOT NULL,
   chat_id serial NOT NULL,
-  room_id integer NOT NULL,
   user_id integer NOT NULL,
   chat_text character varying(1024) NOT NULL,
   chat_time timestamp without time zone NOT NULL,
-  CONSTRAINT chat_pkey PRIMARY KEY (chat_id)
+  CONSTRAINT chat_pkey PRIMARY KEY (chat_id),
+  CONSTRAINT chat_game_id_fkey FOREIGN KEY (game_id)
+      REFERENCES game (game_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
 )
 WITH (
   OIDS=FALSE
