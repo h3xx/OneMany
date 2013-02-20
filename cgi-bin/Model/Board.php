4d154c924d45ed5001aa902318647c18170246fb
Dan Church <amphetamachine@gmail.com>
2013-02-20 16:35:35 -0600

    Add a lot more untested model code.

--- cgi-bin/Model/Board.php
+++ cgi-bin/Model/Board.php
@@ -51,4 +51,247 @@ class ModelBoard {
 
 	}
 
+	public function getBuyFromBankCost ($space_id) {
+		$sth = $this->model->prepare(
+			'select "cost"'.
+			' from "space" where "space_id" = :sid'
+		);
+
+		$sth->bindParam(':sid', $space_id, PDO::PARAM_INT);
+
+		$sth->execute();
+
+		$result = $sth->fetch(PDO::FETCH_NUM);
+
+		return @$result[0];
+	}
+
+	public function whoOwnsSpace ($space_id) {
+		$sth = $this->model->prepare(
+			'select "user_name" from "c_game_space" '.
+			'left join "user" on '.
+			'("user"."user_id" = "c_game_space"."owner_id") '.
+			'where "game_id" = :gid and "space_id" = :sid'
+		);
+
+		$sth->bindParam(':gid', $this->game_id, PDO::PARAM_INT);
+		$sth->bindParam(':sid', $space_id, PDO::PARAM_INT);
+
+		$sth->execute();
+
+		$result = $sth->fetch(PDO::FETCH_NUM);
+
+		return @$result[0];
+	}
+
+	public function rentForSpace ($space_id) {
+		$sth = $this->model->prepare(
+			'select '.
+				'"is_mortgaged", '.
+				'"houses", '.
+				'"space_group" as "group", '.
+				'"foo"."owned_in_group", '.
+				'"rent", '.
+				'"rent1", '.
+				'"rent2", '.
+				'"rent3", '.
+				'"rent4", '.
+				'"rent5" '.
+	'from "c_game_space" '.
+	'left join "space" on ("c_game_space"."space_id" = "space"."space_id") '.
+	'left join ( '.
+		'select "space_group", "owner_id", count(*) as "owned_in_group" from "c_game_space" '.
+			'left join "space" on ("c_game_space"."space_id" = "space"."space_id") '.
+			'where "game_id" = :gid '.
+			'group by "space_group" '.
+	') as "foo" '.
+	'on ("foo"."space_group" = "space"."space_group" '.
+		'and "foo"."owner_id" = "c_game_space"."owner_id") '.
+	'where "c_game_space"."space_id" = :sid'
+		);
+
+		$sth->bindParam(':gid', $this->game_id, PDO::PARAM_INT);
+		$sth->bindParam(':sid', $space_id, PDO::PARAM_INT);
+
+		$sth->execute();
+
+		$result = $sth->fetch(PDO::FETCH_ASSOC);
+
+		if ($result['is_mortgaged']) {
+			# no rent if mortgaged
+			return 0;
+		}
+
+		if (is_numeric($result['group'])) {
+			# regular, buyable property
+			# (XXX : assuming it's owned)
+			switch ($result['houses']) {
+				case 0:
+					return $result['rent'];
+					break;
+					;;
+				case 1:
+				case 2:
+				case 3:
+				case 4:
+				case 5:
+					return $result['rent' . $result['houses']];
+					break;
+					;;
+			}
+		}
+		# FIXME
+	}
+
+	public function housesOnSpace ($space_id) {
+		$sth = $this->model->prepare(
+			'select "houses" from "c_game_space" '.
+			'where "game_id" = :gid and "space_id" = :sid'
+		);
+
+		$sth->bindParam(':gid', $this->game_id, PDO::PARAM_INT);
+		$sth->bindParam(':sid', $space_id, PDO::PARAM_INT);
+
+		$sth->execute();
+
+		$result = $sth->fetch(PDO::FETCH_NUM);
+
+		return @$result[0];
+	}
+
+	public function isPropertyMortgaged ($space_id) {
+		$sth = $this->model->prepare(
+			'select "is_mortgaged" from "c_game_space" '.
+			'where "game_id" = :gid and "space_id" = :sid'
+		);
+
+		$sth->bindParam(':gid', $this->game_id, PDO::PARAM_INT);
+		$sth->bindParam(':sid', $space_id, PDO::PARAM_INT);
+
+		$sth->execute();
+
+		$result = $sth->fetch(PDO::FETCH_NUM);
+
+		return @$result[0];
+	}
+
+	public function rentForHouses ($space_id, $num_houses) {
+		switch ($num_houses) {
+			case 0:
+				$column = 'rent';
+				break;
+			case 1:
+			case 2:
+			case 3:
+			case 4:
+			case 5:
+				$column = 'rent' . $num_houses;
+				break;
+			default:
+				return null;
+		}
+
+		$sth = $this->model->prepare(
+			'select "'.$column.'" from "space" '.
+			'where "space_id" = :sid'
+		);
+
+		$sth->bindParam(':sid', $space_id, PDO::PARAM_INT);
+
+		$sth->execute();
+
+		$result = $sth->fetch(PDO::FETCH_NUM);
+
+		return @$result[0];
+	}
+
+	public function ownedInGroup ($user_id, $group) {
+		$sth = $this->model->prepare(
+			'select '.
+				'count(*) '
+			'from "c_game_space" '.
+				'left join "space" on ("c_game_space"."space_id" = "space"."space_id") '.
+			'where "game_id" = :gid '.
+				'and "owner_id" = :uid '.
+				'and "space_group" = :gr'
+			# (group by clause is unnecessary)
+		);
+
+		$sth->bindParam(':gid', $this->game_id, PDO::PARAM_INT);
+		$sth->bindParam(':uid', $user_id, PDO::PARAM_INT);
+		$sth->bindParam(':gr', $group, PDO::PARAM_STR);
+
+		$sth->execute();
+
+		$result = $sth->fetch(PDO::FETCH_NUM);
+
+		return @$result[0];
+	}
+	
+	public function totalInGroup ($group) {
+		$sth = $this->model->prepare(
+			'select '.
+				'count(*) '
+			'from "space" '.
+			'where "space_group" = :gr'
+			# (group by clause is unnecessary)
+		);
+
+		$sth->bindParam(':gr', $group, PDO::PARAM_STR);
+
+		$sth->execute();
+
+		$result = $sth->fetch(PDO::FETCH_NUM);
+
+		return @$result[0];
+	}
+
+	private function getRentInfo () {
+		$sth = $this->model->prepare(
+			'select '.
+				'"is_mortgaged", '.
+				'"houses", '.
+				'"space_group" as "group", '.
+				'"foo"."owned_in_group", '.
+				'"rent", '.
+				'"rent1", '.
+				'"rent2", '.
+				'"rent3", '.
+				'"rent4", '.
+				'"rent5" '.
+	'from "c_game_space" '.
+	'left join "space" on ("c_game_space"."space_id" = "space"."space_id") '.
+	'left join ( '.
+		'select "space_group", "owner_id", count(*) as "owned_in_group" from "c_game_space" '.
+			'left join "space" on ("c_game_space"."space_id" = "space"."space_id") '.
+			'where "game_id" = :gid '.
+			'group by "space_group" '.
+	') as "foo" '.
+	'on ("foo"."space_group" = "space"."space_group" '.
+		'and "foo"."owner_id" = "c_game_space"."owner_id")'
+		);
+
+		$sth->bindParam(':gid', $this->game_id, PDO::PARAM_INT);
+
+		$sth->execute();
+
+		$result = $sth->fetch(PDO::FETCH_ASSOC);
+
+		return $result;
+	}
+
+	public function isBuyable ($space_id) {
+		$sth = $this->model->prepare(
+			'select "cost" from "space" '.
+			'where "space_id" = :sid'
+		);
+
+		$sth->bindParam(':sid', $space_id, PDO::PARAM_INT);
+
+		$sth->execute();
+
+		$result = $sth->fetch(PDO::FETCH_NUM);
+
+		return isset($result[0]) && $result[0] > -1;
+	}
 }
