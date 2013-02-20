a3a83492a2c257cb2d3e52d2861b7913e3162afa
Dan Church <amphetamachine@gmail.com>
2013-02-20 12:34:53 -0600

    Better chat posting responses.

--- cgi-bin/Controller/Chat.php
+++ cgi-bin/Controller/Chat.php
@@ -9,6 +9,21 @@ class ControllerChat {
 	}
 
 	public function postChatMessage ($chat_text) {
-		$this->model->chat->addChatMessage($chat_text);
+		# FIXME : check if user is in game
+		if ($this->model->user->isValidUserId($this->user_id)) {
+
+			$chat_id = $this->model->chat->addChatMessage($this->user_id, $chat_text);
+			$result = [
+				'result'	=> true,
+				'newstate'	=> $chat_id,
+			];
+		} else {
+			$result = [
+				'result'	=> false,
+				'newstate'	=> -1,
+			];
+		}
+
+		return json_encode($result, JSON_UNESCAPED_UNICODE);
 	}
 }
