27973326599d3d0409ad6863cad6ace38f8519cb
Dan Church <amphetamachine@gmail.com>
2013-02-21 13:27:42 -0600

    Improve user-game relation.

--- db/09create-c_user_game.sql
+++ db/09create-c_user_game.sql
@@ -6,9 +6,14 @@ CREATE TABLE c_user_game
 (
   user_id integer NOT NULL,
   game_id integer NOT NULL,
+  on_space integer NOT NULL DEFAULT 0,
+  cash integer NOT NULL,
   CONSTRAINT c_user_game_pkey PRIMARY KEY (user_id, game_id),
   CONSTRAINT c_user_game_game_id_fkey FOREIGN KEY (game_id)
       REFERENCES game (game_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT c_user_game_on_space_fkey FOREIGN KEY (on_space)
+      REFERENCES space (space_id) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE NO ACTION,
   CONSTRAINT c_user_game_user_id_fkey FOREIGN KEY (user_id)
       REFERENCES "user" (user_id) MATCH SIMPLE
