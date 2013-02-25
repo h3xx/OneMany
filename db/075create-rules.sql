3c7660f99f73ef01231a9d6990da5923bea18346
Dan Church <amphetamachine@gmail.com>
2013-02-25 08:13:19 -0600

    Add a new column to the rules table.

--- db/075create-rules.sql
+++ db/075create-rules.sql
@@ -6,6 +6,7 @@ CREATE TABLE rules
 (
   rule_name character varying(255) NOT NULL,
   rule_default character varying(255) NOT NULL,
+  rule_desc character varying(255),
   CONSTRAINT rules_pkey PRIMARY KEY (rule_name)
 )
 WITH (
--- db/data-rules.sql
+++ db/data-rules.sql
@@ -1,4 +1,11 @@
 -- Table: rules
 
-insert  into "rules"("rule_name","rule_default") values
-	('starting_cash',	'1500');
+insert  into "rules"("rule_name","rule_default","rule_desc") values
+	('starting_cash',	'1500',		'How much cash each player starts with.'),
+	('go_salary',		'200',		'How much cash the player gets for passing GO.'),
+	('free_parking',	'0',		'Whether "Free Parking" is a thing.'),
+	('auctions',		'1',		'Whether unbought properties may be auctioned by the bank upon landing on the space.'),
+	('incometax_flat',	'200',		'How much Income Tax charges (flat rate option).'),
+	('incometax_perc',	'10',		'How much Income Tax charges (percentage option).'),
+	('buy_gojf_card',	'1',		'Whether players may purchase a "Get out of Jail Free" card from another player.'),
+	('jail_bail',		'50',		'How much a player pays to get out of jail.');
