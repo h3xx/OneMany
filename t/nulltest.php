2b5feec1d8b215132f306623986107a6a2894e48
Dan Church <amphetamachine@gmail.com>
2013-02-26 15:35:18 -0600

    Modify accessor test to demonstrate weirdness.

--- t/nulltest.php
+++ t/nulltest.php
@@ -1,6 +1,9 @@
 <?php
 
 class Foo {
+	private $baz;
+	public $blarg;
+
 	function __construct () {}
 
 	private function getBar () {
@@ -11,16 +14,48 @@ class Foo {
 		return $this->bar;
 	}
 
-	public function poop() {
-		print("Running poop()\n");
-		print('bar is ' . $this->bar . "\n");
+	private function getBaz () {
+		print("==Running getBaz()\n");
+		if (!isset($this->baz)) {
+			$this->baz = 5;
+		}
+		return $this->baz;
+	}
+
+	private function getBlarg () {
+		print("==Running getBlarg()\n");
+		if (!isset($this->blarg)) {
+			$this->blarg = 5;
+		}
+		return $this->blarg;
+	}
+
+	public function pubfunc1() {
+		print("==Running pubfunc1()\n");
+		print('==bar is ' . $this->bar . "\n");
+	}
+
+	public function pubfunc2() {
+		print("==Running pubfunc2()\n");
+		print('==baz is ' . $this->baz . "\n");
+	}
+
+	public function pubfunc3() {
+		print("==Running pubfunc3()\n");
+		print('==blarg is ' . $this->blarg . "\n");
 	}
 
 	function __get ($name) {
-		print("Running __get($name)\n");
+		print("==Running __get($name)\n");
 		switch ($name) {
 			case 'bar':
 				return $this->getBar();
+				;;
+			case 'baz':
+				return $this->getBaz();
+				;;
+			case 'blarg':
+				return $this->getBlarg();
 				;;
 		}
 	}
@@ -28,4 +63,18 @@ class Foo {
 
 $f = new Foo();
 
-$f->poop();
+# test 1 : non-declared member
+print("test1: Two subsequent calls:\n");
+$f->pubfunc1();
+$f->pubfunc1();
+
+print("\ntest2: Private members only set when accessed publically:\n");
+$f->pubfunc2();
+print("test2: done.\n\n");
+print("test2: publically, baz is " . $f->baz . "\n");
+$f->pubfunc2();
+
+print("\ntest3: Public members never by accessor:\n");
+$f->pubfunc3();
+print("test2: publically, blarg is " . $f->blarg . "\n");
+$f->pubfunc3();
