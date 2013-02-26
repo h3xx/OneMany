5cb9105615073420654bdbeccf6552f3f955a74c
Dan Church <amphetamachine@gmail.com>
2013-02-26 08:38:35 -0600

    Implement mortgaging property.

--- cgi-bin/Controller/Board.php
+++ cgi-bin/Controller/Board.php
@@ -65,7 +65,64 @@ class ControllerBoard {
 	}
 
 	public function mortgageProperty ($space_id) {
-		# FIXME
+		$owner_id = $this->model->game->whoOwnsSpace($space_id);
+		if (!isset($owner_id) || $owner_id !== $this->user_id) {
+			return [
+				'result'=> false,
+				'msg'	=> 'You do not own that property.',
+			];
+		}
+
+		$is_mortgaged = $this->model->game->isPropertyMortgaged($space_id);
+		if ($is_mortgaged) {
+			return [
+				'result'=> false,
+				'msg'	=> 'Property is already mortgaged.',
+			];
+		}
+
+		$numhouses = $this->model->game->board->housesOnSpace($space_id);
+		if (!isset($numhouses)) {
+			return [
+				'result'=> false,
+				'msg'	=> 'Something went horribly wrong finding the number of houses [WTF].',
+			];
+		}
+
+		if ($numhouses > 0) {
+			return [
+				'result'=> false,
+				'msg'	=> 'Cannot mortgage property - sell houses/hotels first.',
+			];
+		}
+
+		if (!$this->model->game->board->setPropertyMortgaged($space_id, true)) {
+			return [
+				'result'=> false,
+				'msg'	=> 'Failed to mortgage property',
+			];
+		}
+
+		$mortgage = $this->model->game->board->getMortgageCost($space_id);
+		if (!isset($mortgage)) {
+			return [
+				'result'=> false,
+				'msg'	=> 'Something went horribly wrong finding the mortgage cost [WTF].',
+			];
+		}
+
+		$new_cash = $this->model->user->addUserCash($this->user_id, $mortgage);
+		if (!is_numeric($new_cash)) {
+			return [
+				'result'=> false,
+				'msg'	=> 'Something went horribly wrong setting the user cash [WTF].',
+			];
+		}
+
+		return [
+			'result'=> true,
+			'msg'	=> 'Sucessfully mortgaged property.',
+		];
 	}
 
 	public function landOnSpace ($space_id) {
