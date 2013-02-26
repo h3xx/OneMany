37077cb04e6ef9e528f6d655b1cfa0fedbe908a9
Dan Church <amphetamachine@gmail.com>
2013-02-26 08:06:41 -0600

    Reformat return arrays.

--- cgi-bin/Controller/Board.php
+++ cgi-bin/Controller/Board.php
@@ -13,32 +13,55 @@ class ControllerBoard {
 
 		$space_cost = $this->model->board->getBuyFromBankCost($space_id);
 		if (!isset($space_cost) || $space_cost < 0) {
-			return ['result'=>false,'msg'=>'Property is not buyable.'];
+			return [
+				'result'=> false,
+				'msg'	=> 'Property is not buyable.',
+			];
 		}
 
 		$owner = $this->model->board->whoOwnsSpace($space_id);
 		if (isset($owner)) {
 			if ($owner === $this->user_id) {
-				return ['result'=>false,'msg'=>'You already own this property.'];
+				return [
+					'result'=> false,
+					'msg'	=> 'You already own this property.',
+				];
 			}
-			return ['result'=>false,'msg'=>'Property is owned by ' . $this->model->user->resolveUserId($owner) . '.'];
+			return [
+				'result'=> false,
+				'msg'	=> 'Property is owned by ' . $this->model->user->resolveUserId($owner) . '.',
+			];
 		}
 
 		$cash_on_hand = $this->model->user->getUserCash($this->user_id);
 
 		if ($cash_on_hand < $space_cost) {
-			return ['result'=>false,'msg'=>'You do not have enough cash to buy this property.'];
+			return [
+				'result'=> false,
+				'msg'	=> 'You do not have enough cash to buy this property.',
+			];
 		}
 
 		# everything seems fine - the user can buy it
 
 		if (!$this->model->board->setPropertyOwner($space_id, $this->user_id)) {
-			return ['result'=>false,'msg'=>'Failed to set property owner [WTF].'];
+			return [
+				'result'=> false,
+				'msg'	=> 'Failed to set property owner [WTF].',
+			];
 		}
+
 		if (!$this->model->user->setUserCash($this->user_id, $cash_on_hand - $space_cost)) {
-			return ['result'=>false,'msg'=>'Failed to set user cash [WTF].'];
+			return [
+				'result'=> false,
+				'msg'	=> 'Failed to set user cash [WTF].',
+			];
 		}
-		return ['result'=>true,'msg'=>'Successfully bought property.'];
+
+		return [
+			'result'=> true,
+			'msg'	=> 'Successfully bought property.',
+		];
 	}
 
 	public function mortgageProperty ($space_id) {
