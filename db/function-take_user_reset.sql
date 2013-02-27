e67f04734dc12401a7dec73052e2efd107b2147b
Dan Church <amphetamachine@gmail.com>
2013-02-27 14:39:34 -0600

    Fix user password reset function to actually work.

--- db/function-take_user_reset.sql
+++ db/function-take_user_reset.sql
@@ -1,4 +1,4 @@
--- Function: take_user_reset(integer, character varying, text)
+-- Function: take_user_reset(integer, character varying, character varying)
 --
 -- Database engine: PostgreSQL 9.2
 -- Dependencies: `pgcrypto' extension
@@ -8,20 +8,20 @@
 -- @author: Dan Church <h3xx@gmx.com>
 -- @license: GPL v3.0
 
--- DROP FUNCTION take_user_reset(integer, character varying, text);
+-- DROP FUNCTION take_user_reset(integer, character varying, character varying);
 
-CREATE OR REPLACE FUNCTION take_user_reset(_user_id integer, _reset_string character varying, password_plain text)
+CREATE OR REPLACE FUNCTION take_user_reset(_user_id integer, _reset_string character varying, password_plain character varying)
   RETURNS boolean AS
 $BODY$
 
 declare
-	hashy_hash	text;
-	salty_salt	text;
+	_hash	character varying;
+	_salt	character varying;
 
 begin
 
-	select
-		count(*)
+	perform
+		"user_id"
 		from "user"
 		where
 		"user_id" = _user_id and
@@ -34,14 +34,14 @@ begin
 	end if;
 
 	-- blowfish salt = 128 bits = 16 characters
-	select into salty_salt gen_salt('bf');
-	select into hashy_hash encode(digest(crypt(password_plain, salty_salt), 'sha1'), 'hex');
+	select into _salt gen_salt('bf');
+	select into _hash encode(digest(crypt(password_plain, _salt), 'sha1'), 'hex');
 
 	update
 		"user"
 		set
-		"login_hash" = hashy_hash,
-		"login_salt" = salty_salt,
+		"login_hash" = _hash,
+		"login_salt" = _salt,
 		"reset_string" = null,
 		"reset_expire" = null
 
