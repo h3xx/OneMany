c6ad1c3014c48eac5dfaa092a8494c8fbbbe99a9
Dan Church <amphetamachine@gmail.com>
2013-02-27 15:09:08 -0600

    Improve password resetting.

--- cgi-bin/Controller/Login.php
+++ cgi-bin/Controller/Login.php
@@ -16,6 +16,7 @@ class ControllerLogin {
 		switch (@$buff[0]) {
 			case 'reset':
 				if (isset($buff[2])) {
+					return $this->doResetPassword($buff[1], $buff[2], $buff[3]);
 
 				}
 				break;
@@ -48,16 +49,18 @@ class ControllerLogin {
 		$user_id = $this->model->user->resolveUserEmail($user_email);
 
 		if (!isset($user_id)) {
+			$response['msg'] .= ' no uid'; # debugging
 			return $response;
 		}
 
 		$rst = $this->model->user->addPwResetRequest($user_id);
 
 		if (!isset($rst)) {
+			$response['msg'] .= ' request failed'; # debugging
 			return $response;
 		}
 
-		$url = self::$rst_url . '?args=reset:'.$user_id.':'.urlencode($rst);
+		$url = self::$rst_url . '?args='.$user_id.':'.urlencode($rst);
 
 		$subject = 'OneMany Password Reset';
 		$headers = [
@@ -66,20 +69,28 @@ class ControllerLogin {
 		];
 		$content = '<a href="'.htmlspecialchars($url).'">Reset your password</a>';
 
-		if (!mail($user_email, $subject, $content, implode("\r\n", $headers))) {
-			return $response;
-		}
+		#if (!mail($user_email, $subject, $content, implode("\r\n", $headers))) {
+		#	$response['msg'] .= ' mailing failed'; # debugging
+		#	return $response;
+		#}
 
-		$response['msg'] .= 'foo'; # debugging
+		$response['msg'] .= ' ' . $url; # debugging
 
 		return $response;
 	}
 
 	public function doResetPassword ($user_id, $reset_string, $new_pass) {
 		$result = $this->model->user->doPwReset($user_id, $reset_string, $new_pass);
+		if (!$result) {
+			return [
+				'result'=> false,
+				'msg'	=> 'Failed to set password.',
+			];
+		}
 
 		return [
-			'result'=> $result,
+			'result'=> true,
+			'msg'	=> 'Successfully set new password.',
 		];
 	}
 }
