6c8c2fdc39064c26c2a68938e439e3928b4ec9a1
Dan Church <amphetamachine@gmail.com>
2013-02-28 09:03:38 -0600

    Add last_roll column to game table.

--- db/01create-game.sql
+++ db/01create-game.sql
@@ -6,8 +6,11 @@ CREATE TABLE game
 (
   game_id serial NOT NULL,
   game_name character varying(1024) NOT NULL,
+  last_roll integer[], -- Last roll performed.
   CONSTRAINT game_pkey PRIMARY KEY (game_id)
 )
 WITH (
   OIDS=FALSE
 );
+
+COMMENT ON COLUMN game.last_roll IS 'Last roll performed.';
