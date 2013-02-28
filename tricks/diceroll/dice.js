7d64753b961adae862d399244980367846654199
Dan Church <amphetamachine@gmail.com>
2013-02-28 12:43:29 -0600

    Update dice trick.

--- tricks/diceroll/dice.js
+++ tricks/diceroll/dice.js
@@ -1,35 +1,77 @@
 $(document).ready(function () {
-	window.poop = $('#dice')
+	var dice_elems = [$('#dice1'), $('#dice2')];
+	for (var d in dice_elems) {
+		dice_elems[d]
 		//.hide()
 		.dice({
-			'id': '#dice',
+			/*
 			'callback': function (num) {
 				$('#currentnumber').html('Current number: '+num);
 
-			},
+			},*/
 			'glyphSrc': 'images/dice.png',
 			'glyphSize': 67.667,
 			'juggleTimeout': 10000,
 		});
+	}
 
 	$('#roll')
 		.button()
 		.click(function (e) {
-			$('#dice')
+			for (var d in dice_elems) {
+				dice_elems[d]
 				.dice({
 					'running': 1,
 					'number': -1,
 				});
+			}
+			$.post('responder.php', {
+				'method': 'tell',
+				'func': 'game',
+				'args': 'roll',
+			},
+			function (data) {
+				if (!data.result) {
+					$('#currentnumber').text('failed: ' + data.msg);
+					dice_elems[0].dice({'running': 0});
+					dice_elems[1].dice({'running': 0});
+				}
+			});
+
 
 		});
-	$('#stop')
+
+	window.gamestate = 0;
+	$('#poll')
 		.button()
 		.click(function (e) {
-			$('#dice')
-				.dice({
-					'running': 0,
-					'number': Math.floor(Math.random() * 6) + 1,
-				});
+			$.post('responder.php', {
+				'method': 'ask',
+				'func': 'pollGame',
+				'args': window.gamestate,
+			},
+			function (data) {
+				if (!data && !data.instructions) {
+					return;
+				}
+
+				for (var i in data.instructions) {
+					procGameUpdate(data.instructions[i]);
+				}
+
+				if (window.gamestate < data.newstate) {
+					window.gamestate = data.newstate;
+				}
+			});
 
 		});
 });
+
+function procGameUpdate (update) {
+	upd = jQuery.parseJSON(update);
+	if (upd.type == 'roll') {
+		$('#dice1').dice({running:0,number:upd.val[0]});
+		$('#dice2').dice({running:0,number:upd.val[1]});
+	}
+	$('#currentnumber').append(upd.val[0]);
+}
