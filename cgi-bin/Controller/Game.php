cc3254542bcec19d89c9140653be784b1dec02dc
Dan Church <amphetamachine@gmail.com>
2013-03-01 09:41:03 -0600

    Implement more game logic
    * Doubles-induced jail
    * Split off controller functionality
    * User turn rotation

--- cgi-bin/Controller/Game.php
+++ cgi-bin/Controller/Game.php
@@ -44,6 +44,20 @@ class ControllerGame {
 	}
 
 	private function rollDice () {
+		# check whether it's their turn
+		$whose_turn = $this->model->game->whoseTurn();
+		if ($whose_turn !== $this->user_id) {
+			return [
+				'result'=> false,
+				'msg'	=> 'Not your turn.',
+			];
+		}
+
+		# what to return upon successful roll
+		$success = [
+			'result'=> true,
+			'msg'	=> 'Successfully rolled dice.',
+		];
 
 		$roll = $this->model->game->doRoll($this->user_id, 2);
 
@@ -54,11 +68,84 @@ class ControllerGame {
 			];
 		}
 
+		if ($this->model->user->isInJail($this->user_id)) {
+			# user is in jail
+			if ($roll[0] === $roll[1]) {
+				# doubles - get out of jail
+				$this->model->user->setInJail($this->user_id, false);
+			}
+			# TODO : only be able to roll three times to get freed from jail ?
+			return $success;
+		} else {
+			if ($roll[0] === $roll[1]) {
+				# doubles
+				$num_doubles = $this->model->user->incrementDoubles($this->user_id);
+				if (!is_numeric($num_doubles)) {
+					return [
+						'result'=> false,
+						'msg'	=> 'Failed to set the number of doubles.',
+					];
+				}
+				$jail_doubles = $this->model->rules->getRuleValue('jail_doubles', 3);
+
+				if ($jail_doubles !== 0 && $num_doubles >= $jail_doubles) {
+					# doubles-induced jail
+					return $this->throwUserInJail($success);
+				}
+
+				# you rolled doubles, but you're not going to
+				# jail for it - still your turn after this
+				# FIXME : implement
+			} else {
+				# no doubles - your turn is over after this
+				# FIXME : implement
+			}
+		}
+
 		# FIXME : check turn, move piece, etc.
-		return [
-			'result'=> true,
-			'msg'	=> 'Successfully rolled dice.',
-		];
+		return $success;
+	}
+
+	private function throwUserInJail ($success_return) {
+		# figure out where we need to send the user
+		$jail_ids = $this->model->game->board->getSpaceIdsInGroup('JV');
+		if (!is_array($jail_ids) || !is_numeric($jail_ids[0])) {
+			return [
+				'result'=> false,
+				'msg'	=> 'Trying to throw you in jail, but no idea where jail is [WTF].',
+			];
+		}
+
+		$jail_id = $jail_ids[0];
+
+		# move the user to jail
+		if (!$this->model->user->moveToSpace($this->user_id, $jail_id)) {
+			return [
+				'result'=> false,
+				'msg'	=> 'Failed to move you to jail [WTF].',
+			];
+		}
+		# throw the user in jail
+		if (!$this->model->user->setInJail($this->user_id, false)) {
+			return [
+				'result'=> false,
+				'msg'	=> 'Failed to put you in jail [WTF].',
+			];
+		}
+
+		return $this->turnIsOver($success_return);
+	}
+	
+	private function turnIsOver ($success_return) {
+		# your turn is over
+		if (!is_numeric($this->model->game->rotateTurn())) {
+			return [
+				'result'=> false,
+				'msg'	=> 'Failed to rotate turn [WTF].',
+			];
+		}
+
+		return $success_return;
 	}
 
 	function __get ($name) {
