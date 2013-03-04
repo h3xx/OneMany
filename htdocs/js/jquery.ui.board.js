dcaa9ce9ed92b8bfc77f0248c793e033b1d4b4a9
Dan Church <amphetamachine@gmail.com>
2013-03-04 11:19:09 -0600

    Add more to board layout.

--- htdocs/js/jquery.ui.board.js
+++ htdocs/js/jquery.ui.board.js
@@ -12,6 +12,158 @@ $.widget("ui.board", {
 		return this.uiBoard;
 	},
 
+	makePieces: function () {
+		var self = this,
+		piec = $('<table></table>')
+			.addClass('ui-board-pieces'),
+		pbody = $('<tbody></tbody>');
+
+		piec.append(pbody);
+
+		var id = 0;
+
+		// first row
+		var row1 = $('<tr></tr>');
+		pbody.append(row1);
+
+		// GO
+		row1.append(
+			$('<td></td>')
+				.addClass('propCrnr propMain go')
+				.append(
+					(self.displays['id' + id++] = {}).pieces =
+					$('<div></div>').addClass('iconsCorner')
+				)
+		);
+
+		for (var x = 0; x < 9; ++x) {
+			var cell = $('<td></td>')
+				.addClass('propVert propMain')
+				// outer edge
+				.append(
+					(self.displays['id' + id] = {}).pieces =
+					$('<div></div>').addClass('iconsVert')
+				)
+				// spacing
+				.append(
+					$('<div></div>').addClass('vert')
+				)
+				// houses space
+				.append(
+					(self.displays['id' + id++] = {}).houses =
+					$('<div></div>').addClass('iconsVert')
+				);
+
+			row1.append(cell);
+		}
+
+		// Jail
+		row1.append(
+			$('<td></td>')
+				.addClass('propCrnr propMain jail')
+				.append(
+					(self.displays['id' + id++] = {}).pieces =
+					$('<div></div>').addClass('iconsCorner')
+				)
+		);
+
+		// rows 2-9
+		for (var x = 0; x < 9; ++x) {
+			var rowx = $('<tr></tr>');
+
+			var cell1 = $('<td></td>')
+				.addClass('propHorz propMain')
+				// outer edge
+				.append(
+					(self.displays['id' + id] = {}).pieces =
+					$('<div></div>').addClass('iconsHorz')
+				)
+				// spacing
+				.append(
+					$('<div></div>').addClass('horz')
+				)
+				// houses space
+				.append(
+					(self.displays['id' + id++] = {}).houses =
+					$('<div></div>').addClass('iconsHorz')
+				),
+
+			cell2 = $('<td></td>')
+				.attr('colspan', '9'),
+
+			cell3 = $('<td></td>')
+				.addClass('propHorz propMain')
+				// houses space
+				.append(
+					(self.displays['id' + id] = {}).houses =
+					$('<div></div>').addClass('iconsHorz')
+				)
+				// spacing
+				.append(
+					$('<div></div>').addClass('horz')
+				)
+				// outer edge
+				.append(
+					(self.displays['id' + id++] = {}).pieces =
+					$('<div></div>').addClass('iconsHorz')
+				);
+
+			pbody.append(
+				rowx
+					.append(cell1)
+					.append(cell2)
+					.append(cell3)
+			);
+		}
+
+		// bottom row
+		var row10 = $('<tr></tr>');
+		pbody.append(row10);
+
+		// Go To Jail
+		row10.append(
+			$('<td></td>')
+				.addClass('propCrnr propMain')
+				.append(
+					(self.displays['id' + id++] = {}).pieces =
+					$('<div></div>').addClass('iconsCorner')
+				)
+		);
+
+		for (var x = 0; x < 9; ++x) {
+			var cell = $('<td></td>')
+				.addClass('propVert propMain')
+				// outer edge
+				.append(
+					(self.displays['id' + id] = {}).houses =
+					$('<div></div>').addClass('iconsVert')
+				)
+				// spacing
+				.append(
+					$('<div></div>').addClass('vert')
+				)
+				// houses space
+				.append(
+					(self.displays['id' + id++] = {}).pieces =
+					$('<div></div>').addClass('iconsVert')
+				);
+
+			row10.append(cell);
+		}
+
+		// Free Parking
+		row10.append(
+			$('<td></td>')
+				.addClass('propCrnr propMain')
+				.append(
+					(self.displays['id' + id++] = {}).pieces =
+					$('<div></div>').addClass('iconsCorner')
+				)
+		);
+
+		return piec;
+	},
+
 	makeBoard: function () {
 		var self = this,
 		brd = $('<div></div>')
@@ -28,7 +180,8 @@ $.widget("ui.board", {
 
 		brd
 			.append(bg)
-			.append(nam);
+			.append(nam)
+			.append(self.makePieces());
 
 		return brd;
 	},
