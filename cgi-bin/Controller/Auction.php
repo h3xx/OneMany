7e27bcac4ca97609bb81b48fbdb16b2245f9e4e5
Dan Church <amphetamachine@gmail.com>
2013-03-05 17:29:51 -0600

    More auction tests.

--- cgi-bin/Controller/Auction.php
+++ cgi-bin/Controller/Auction.php
@@ -9,6 +9,7 @@ class ControllerAuction {
 	}
 
 	public function startAuction ($space_id) {
+		# FIXME : check turn
 		if (!isset($space_id)) {
 			$space_id = $this->model->game->getUserOnSpace($this->user_id);
 			if (!is_numeric($space_id)) {
@@ -19,10 +20,31 @@ class ControllerAuction {
 			}
 		}
 
+		if (!$this->model->game->board->isOwnable($space_id)) {
+			return [
+				'result'=> false,
+				'msg'	=> 'Space is not ownable.',
+			];
+		}
+
 		# FIXME : implement
 	}
 
 	public function addBid ($space_id, $bid) {
+		if (!$this->model->game->board->isOwnable($space_id)) {
+			return [
+				'result'=> false,
+				'msg'	=> 'Space is not ownable.',
+			];
+		}
+		$ainfo = $this->model->game->getAuctionInfoNoExpired();
+		if (empty($ainfo) || $ainfo['aspace'] !== $space_id) {
+			return [
+				'result'=> false,
+				'msg'	=> 'Space is not up for auction.',
+			];
+		}
+
 		# FIXME : implement
 
 	}
