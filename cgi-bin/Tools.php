f21eb00d005b1846b63fdef587c6b87b1a3d69c4
Dan Church <amphetamachine@gmail.com>
2013-03-06 08:34:31 -0600

    Add function to make a url to our toolbox.

--- cgi-bin/Tools.php
+++ cgi-bin/Tools.php
@@ -2,8 +2,18 @@
 
 class Tools {
 	public static $can_mail = false;
+	public static $htdocs_path = '/htdocs'; # may be empty in production
 
 	public static function encodeJson ($json_data) {
 		return json_encode($json_data, JSON_UNESCAPED_UNICODE);
+	}
+
+	public static function absUrl ($subpath) {
+		return sprintf('%s://%s%s%s',
+			$_SERVER['REQUEST_SCHEME'],
+			$_SERVER['HTTP_HOST'],
+			self::$htdocs_path,
+			$subpath
+		);
 	}
 }
