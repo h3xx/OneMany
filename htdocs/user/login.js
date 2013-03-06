faf4393a3eeea997ed8385adfacf2c58679dfeeb
Dan Church <amphetamachine@gmail.com>
2013-03-06 09:30:40 -0600

    Add login form.

---
+++ htdocs/user/login.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+	var frmelems = [
+		$('#logname'),
+		$('#pw'),
+		$('#login'),
+	];
+	
+	$("#progressbar")
+		.progressbar({
+			value: false,
+		})
+		.hide();
+
+	$('#login')
+		.button()
+		.click(function(e) {
+			e.preventDefault();
+			var loginvars = 'login:' + $('#logname').val() + ':' + $('#pw').val();
+
+			for (var i in frmelems) {
+				frmelems[i].attr('disabled', 'disabled');
+			}
+			$('#progressbar').show(500);
+
+			$.post('responder.php', {
+				'method': 'tell',
+				'func': 'login',
+				'args': loginvars,
+			}, function (data) {
+				$('#progressbar').hide(500);
+				$('#result').text(data.msg);
+				if (!data.result) {
+					for (var i in frmelems) {
+						frmelems[i].removeAttr('disabled');
+					}
+				}
+			});
+		});
+});
---
+++ htdocs/user/login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Login Form</title>
+<link rel="stylesheet" href="../css/jquery-ui-1.10.1.css" type="text/css" media="screen" />
+<script type="text/javascript" src="../js/jquery-1.9.1.min.js"></script>
+<script type="text/javascript" src="../js/jquery-ui-1.10.1.min.js"></script>
+<script type="text/javascript" src="login.js"></script>
+<link rel="stylesheet" type="text/css" href="userform.css" />
+</head>
+<body>
+<h1>Login Form</h1>
+<form>
+<input id="logname" type="text" name="logname" placeholder="Username" />
+<input id="pw" type="password" name="pw" placeholder="Password" />
+<input id="login" type="submit" name="login" value="Login" />
+</form>
+<div id="progressbar"></div>
+<div id="result"></div>
+</body>
+</html>
