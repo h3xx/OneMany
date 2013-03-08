efaa0a630988a7d27462aeb42a46d2e7c989b07a
Dan Church <amphetamachine@gmail.com>
2013-03-08 12:25:38 -0600

    Overhaul game listing.

--- htdocs/list/gameslist.js
+++ htdocs/list/gameslist.js
@@ -2,17 +2,25 @@ $(document).ready(function () {
 	var glist =
 		$('#games')
 		.addClass('ui-widget'),
+	lia =
+		$('#loggedinas')
+		.text('Getting your user info.'),
+	gli =
+		$('#gotologin')
+		.hide(),
+
+	loggedIn = false,
+
+	gamename = $('#gamename'),
 	pbar = $("#progressbar")
 		.progressbar({
 			value: false,
 		})
-		.hide();
+		.show(500),
 
-	pbar.show(500);
+	allFields = $([]).add(gamename),
 
-	var allFields = $([]).add($('gamename'));
-
-	$('#dialog-form')
+	dialogform = $('#dialog-form')
 		.dialog({
 			autoOpen: false,
 			height: 300,
@@ -21,9 +29,9 @@ $(document).ready(function () {
 			buttons: {
 				'Create Game': function () {
 					var self = $(this),
-					gn = $('#gamename').val();
+					gn = gamename.val();
 					if (!gn) {
-						$('#gamename').addClass('ui-state-error');
+						gamename.addClass('ui-state-error');
 						return;
 					}
 					self.dialog('close');
@@ -36,84 +44,125 @@ $(document).ready(function () {
 				allFields.val('').removeClass('ui-state-error');
 
 			}
-		});
+		}),
 
-	$('#create-game')
+	creategame = $('#create-game')
 		.text('Create Game')
 		.button()
 		.click(function() {
 			$('#dialog-form').dialog('open');
-		});
+		}),
 
-	$.post('responder.php', {
-		'method': 'ask',
-		'func': 'listGames',
-	}, function (data) {
-		pbar.hide(500);
-		if (data) {
-			glist.empty();
-			glist.append(
-				$('<div></div>')
-				.addClass('row ui-widget-header')
-				.append(
-					$('<div></div>')
-					.addClass('cell')
-					.text('ID'),
+	// the elements that are to be disabled if not logged in
+	nliDisable = $([]).add(creategame),
 
-					$('<div></div>')
-					.addClass('cell')
-					.text('Name'),
-
-					$('<div></div>')
-					.addClass('cell')
-					.text('Players'),
-
-					$('<div></div>')
-					.addClass('cell')
-				)
-			);
-			for (var i in data) {
-				var gdata = data[i];
-
+// createGamesList {{{
+	createGamesList = function () {
+		$.post('responder.php', {
+			'method': 'ask',
+			'func': 'listGames',
+		}, function (data) {
+			pbar.hide(500);
+			if (data) {
+				glist.empty();
 				glist.append(
 					$('<div></div>')
-					.addClass('row ui-widget-content')
+					.addClass('row ui-widget-header')
 					.append(
 						$('<div></div>')
 						.addClass('cell')
-						.text(gdata.id),
+						.text('ID'),
 
 						$('<div></div>')
 						.addClass('cell')
-						.text(gdata.name),
+						.text('Name'),
 
 						$('<div></div>')
 						.addClass('cell')
-						.text(gdata.sz),
+						.text('Players'),
 
-						$('<button></button>')
+						$('<div></div>')
 						.addClass('cell')
-						.button()
-						.click(function () {
-							var gid = $(this).data('gid');
-							$.post('responder.php',
-								{
-									method: 'tell',
-									func: 'join',
-									args: gid,
-								},
-								function (data) {
-									if (!data.result) {
-										alert(data.msg);
-									}
-								});
-
-						})
-						.data('gid', gdata.id)
-						.text('Join')
 					)
 				);
+				for (var i in data) {
+					var gdata = data[i], joinbtn;
+
+					glist.append(
+						$('<div></div>')
+						.addClass('row ui-widget-content')
+						.append(
+							$('<div></div>')
+							.addClass('cell')
+							.text(gdata.id),
+
+							$('<div></div>')
+							.addClass('cell')
+							.text(gdata.name),
+
+							$('<div></div>')
+							.addClass('cell')
+							.text(gdata.sz),
+
+							joinbtn =
+							$('<button></button>')
+							.addClass('cell')
+							.button()
+							.click(function () {
+								var gid = $(this).data('gid');
+								$.post('responder.php',
+									{
+										method: 'tell',
+										func: 'join',
+										args: gid,
+									},
+									function (data) {
+										if (!data.result) {
+											alert(data.msg);
+										}
+									});
+
+							})
+							.data('gid', gdata.id)
+							.text('Join')
+						)
+					);
+
+					nliDisable.add(joinbtn);
+					if (!loggedIn) {
+						joinbtn.attr('disabled', 'disabled');
+					}
+				}
 			}
-		}
-	});
+
+			/* this should work but it doesn't */
+			if (!loggedIn) {
+				gli.show();
+				nliDisable.attr('disabled', 'disabled');
+			}
+		});
+	};
+// createGamesList }}}
+
+	$.post('responder.php',
+		{
+			'method': 'ask',
+			'func': 'myUserInfo',
+		}, function (data) {
+			if (data) {
+				if (data.name) {
+					lia.text('You are logged in as ' + data.name);
+					loggedIn = true;
+				} else if (!data.result) {
+					lia.text(data.msg);
+				}
+			} else {
+				lia.text('Failure' + data);
+			}
+			createGamesList();
+		});
+
+
 });
+
+// vi: fdm=marker
