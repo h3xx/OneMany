d9c0ac3d3426fae35ba7df9f9721dce38bc2a783
Dan Church <amphetamachine@gmail.com>
2013-03-08 13:00:10 -0600

    Improve progress bar syntax.
    Obsoletes hacky xhtml animations.

--- tricks/animated-progress-bar/progbar.js
+++ tricks/animated-progress-bar/progbar.js
@@ -1,36 +1,57 @@
 $(document).ready(function () {
 	//Progressbar initialization
-	$("#pbar").progressbar({
-		value: 0,
-//		change: function() {
-//
-//		},
-	});
+	var
+	intervalId,
+	pbar = $("#pbar")
+		.progressbar({
+			value: 0,
+		}),
+
 	//Button click event
-	$("#oper").click(function (e) {
-		//Disabling button
-		$("#oper").attr('disabled', 'disabled');
-		//Making sure that progress indicate 0
-		$("#pbar").progressbar('value', 0);
-		//Perform POST for triggering long running operation
-		$.post('poll.php?start=1', function (data) {
-			//Updating progress
-			$("#pbar").progressbar('value', data.progress);
-			//Setting the timer
-			window.progressIntervalId = window.setInterval(function () {
-				//Getting current operation progress
-				$.get('poll.php', function (data) {
-					//Updating progress
-					$("#pbar").progressbar('value', data.progress);
-					//If operation is complete
-					if (data.progress == 100) {
-						//Clear timer
-						window.clearInterval(window.progressIntervalId);
-						//Enable button
-						$("#pbar").attr('disabled', '');
-					}
-				});
+	oper = $("#oper")
+		.click(function (e) {
+			//Disabling button
+			oper.attr('disabled', 'disabled');
+
+			//Making sure that progress indicate 0
+			pbar
+				.data('val', 0)
+				.progressbar('value', 0);
+
+			//Perform POST for triggering long running operation
+			$.post('poll.php?start=1', function (data) {
+				//Updating progress
+				pbar
+					.data('val', data.progress)
+					.progressbar('value', data.progress);
+				//Setting the timer
+				intervalId = window.setInterval(function () {
+					//Getting current operation progress
+					$.get('poll.php', function (data) {
+						//Updating progress
+						pbar
+							.data('val', data.progress)
+							.progressbar('value', data.progress);
+
+						//If operation is complete
+						if (data.progress >= 100) {
+							//Clear timer
+							window.clearInterval(intervalId);
+							//Enable button
+							//$("#pbar").attr('disabled', '');
+						}
+					});
+				}, 500);
+			});
+		}),
+
+	noper = $('#noper')
+		.click(function (e) {
+			window.clearInterval(intervalId);
+			/*
+			intervalId = window.setInterval(function () {
+				pbar.progressbar('value', pbar.data('val'));
 			}, 500);
+			*/
 		});
-	});
 });
